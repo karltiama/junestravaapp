5be24aae16875f1c8da040ad099117ca74de2279
Karl Tiama <karltiama@gmail.com>
2024-07-25 23:47:17 -0400

    feat: Add Fitness component and configure SVG loader

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,25 @@
-`use client`;
+"use client ";
 
+import { Button } from "@/components/ui/button";
+
+import TestStrava from "@/components/text-strava";
 import Link from "next/link";
+import MySVGIcon from "@/components/fitness";
+import { FitnessSvg } from "@/components/fitnessSvg";
 
 const authorizeUrl = `https://www.strava.com/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_STRAVA_CLIENT_ID}&response_type=code&redirect_uri=${process.env.NEXT_PUBLIC_STRAVA_REDIRECT_URI}&approval_prompt=force&scope=read,activity:read,activity:read_all`;
 
 export default function Home() {
 	return (
-		<main className="flex min-h-screen flex-col items-center justify-between p-24">
-			<h1>Strava API Test</h1>
-			<h1>Connect with Strava</h1>
+		<main className="flex min-h-screen items-center justify-evenly p-16">
 			<Link href={authorizeUrl}>
-				<button>Authorize with Strava</button>
+				<Button>Authorize with Strava</Button>
 			</Link>
+			<div className="w-2/5">
+				<FitnessSvg />
+			</div>
+
+			{/* <TestStrava /> */}
 		</main>
 	);
 }
---
+++ src/components/fitness.tsx
@@ -0,0 +1,9 @@
+import FitnessIcon from "../assets/fitness.svg";
+
+const MySVGIcon = () => (
+	<div>
+		<FitnessIcon width="300" height="300" />
+	</div>
+);
+
+export default MySVGIcon;
