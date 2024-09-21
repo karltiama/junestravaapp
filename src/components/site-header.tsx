a60894370dc033b691d0076eefe31c64a68b529b
Karl Tiama <karltiama@gmail.com>
2024-09-21 12:58:55 -0400

    refactor: add logo to header

--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -5,16 +5,19 @@ import { Button } from "./ui/button";
 export function SiteHeader() {
 	return (
 		<header className="w-full bg-slate-500 z-10">
-			<nav className="max-w-[1400px] mx-auto flex items-center sm:px-16 px-4 py-4">
-				<Link href="/" className="flex justify-center items-center">
-					<Image
-						src="/person-running-solid.svg"
-						alt="Running Logo"
-						width={24}
-						height={24}
-						className="object-contain"
-					/>
-				</Link>
+			<nav className="max-w-[1400px] mx-auto flex items-center justify-between sm:px-16 px-4 py-4">
+				<div className="flex items-center">
+					<Link href="/" className="flex justify-center items-center mr-4">
+						<Image
+							src="/person-running-solid.svg"
+							alt="Running Logo"
+							width={24}
+							height={24}
+							className="object-contain"
+						/>
+					</Link>
+					<h1 className="text-2xl font-bold text-white">Enduro Stats</h1>
+				</div>
 				{/* <Button variant="default">Sign in</Button> */}
 			</nav>
 		</header>
